9b95fedb64c3e5eb748b5df7ef485486397ffa12
kiraaziz <83905214+kiraaziz@users.noreply.github.com>
2023-08-06 08:42:25 -0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,7 @@
-"use client"
-
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-
-import React, { useState } from 'react';
-
 export default () => {
-    const [content, setContent] = useState('');
-    const handleChange = (value) => {
-        setContent(value);
-      };
-      
       return (
         <div>
-          <ReactQuill value={content} onChange={handleChange} />
+          hello
         </div>
       );
       
